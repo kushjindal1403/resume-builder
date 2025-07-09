489a2e373ad25e795c3840e15251c7d68a951c5b
kushjindal1403 <kushjindal1403@gmail.com>
2025-07-09 18:51:34 +0530

    Update navBar.js

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -5,13 +5,23 @@ const Navbar = () => {
         `px-4 py-2 rounded ${isActive ? "bg-blue-600 text-white" : "text-gray-700"}`;
 
     return (
-        <nav className="flex gap-4 p-4 bg-white shadow mb-4">
-            <NavLink to="/form" className={navClass}>Form</NavLink>
-            <NavLink to="/preview" className={navClass}>Preview</NavLink>
-            <NavLink to="/ats-checker" className={navClass}>ATS Score</NavLink>
-            <NavLink to="/templates" className={navClass}>Templates</NavLink>
-            
-        </nav>
+        <>
+            {/* Inline style to hide .no-print elements when printing */}
+            <style>
+                {`@media print {
+                    .no-print {
+                        display: none !important;
+                    }
+                }`}
+            </style>
+
+            <nav className="flex gap-4 p-4 bg-white shadow mb-4 no-print">
+                <NavLink to="/form" className={navClass}>Form</NavLink>
+                <NavLink to="/preview" className={navClass}>Preview</NavLink>
+                <NavLink to="/ats-checker" className={navClass}>ATS Score</NavLink>
+                <NavLink to="/templates" className={navClass}>Templates</NavLink>
+            </nav>
+        </>
     );
 };
 
